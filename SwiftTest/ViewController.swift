7f93596dd99a5fbbb3fe135207b85ca8a71da968
God is a girl <YYDJ@YYDJ.local>
2016-03-17 14:43:01 +0800

    swift

---
+++ SwiftTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTest
+//
+//  Created by Y杨定甲 on 16/2/18.
+//  Copyright © 2016年 damai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
